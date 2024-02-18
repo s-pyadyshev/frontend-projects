35344bf7fc929cfbc306a71de43e3f78ed288993
Sergei Piadyshev <s.pyadyshev@gmail.com>
2024-02-18 17:10:26 +0400

    update

---
+++ media-buying/build/js/media.js
@@ -0,0 +1,55 @@
+const buttonsWrap = document.querySelector(".section-services__buttons");
+const buttons = document.querySelectorAll(".section-services__button");
+const cards = document.querySelectorAll(".services-card");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const targetCardId = button.getAttribute("data-target");
+
+    buttons.forEach((btn, index) => {
+      if (btn !== button) {
+        btn.classList.remove("is-active");
+      }
+      if (index === 0 && btn === button) {
+        buttonsWrap.classList.remove("right");
+        buttonsWrap.classList.add("left");
+      }
+      if (index === 1 && btn === button) {
+        buttonsWrap.classList.remove("left");
+        buttonsWrap.classList.add("right");
+      }
+    });
+    button.classList.add("is-active");
+
+    cards.forEach((card) => {
+      const cardId = card.getAttribute("data-content");
+      if (cardId === targetCardId) {
+        cards.forEach((item) => {
+          if (item !== card) {
+            item.classList.remove("is-active");
+          }
+        });
+        card.classList.add("is-active");
+      }
+    });
+  });
+});
+
+const contactForm = document.querySelector(".contact-form");
+const contactFormHeader = document.querySelector(".contact-form__header");
+
+contactFormHeader.addEventListener("click", () => {
+  contactForm.classList.toggle("is-active");
+});
+
+const videoTitles = document.querySelectorAll(".section-video__inner-title");
+
+let currentIndex = 0;
+
+function toggleActive() {
+  videoTitles.forEach((element) => element.classList.remove("is-active"));
+  currentIndex = (currentIndex + 1) % videoTitles.length;
+  videoTitles[currentIndex].classList.add("is-active");
+}
+
+setInterval(toggleActive, 7000);
